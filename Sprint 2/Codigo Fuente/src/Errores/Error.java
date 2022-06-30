def6a68b7a8e06dea7c3ea66f463f83f9e1b7c95
unknown <tinchocaballero@hotmail.com>
2022-06-30 11:38:44 -0300

    Corregida clase Error y corregidos diagramas de clases

--- Sprint 2/Codigo Fuente/src/Errores/Error.java
+++ Sprint 2/Codigo Fuente/src/Errores/Error.java
@@ -2,43 +2,33 @@ package Errores;
 
 public class Error {
 	
-	private static String error0 = "El medico no trabaja en la obra social tuya.";
-	private static String error1 = "No hay turnos disponibles, desea ver los de la siguiente semana? 1-Si 2-No";
-	private static String error2 = "Datos no confirmados.";
-	private static String error3 = "El numero de turno ingresado no existe.";
-	private static String error4 = "No hay turnos disponibles.";
-	private static String error5 = "El medico seleccionado no existe.";
-	private static String error6 = "No hay medicos disponibles.";
-	private static String error7= "La opcion ingresada no es valida.";
-	private static String error8 = "Usuario y contraseña invalidos. ";
-	
 	public static void getError0() {
-		System.out.println(error0);
+		System.out.println("El medico no trabaja en la obra social tuya.");
 	}
 	
 	public static void getError1() {
-		System.out.println(error1);
+		System.out.println("No hay turnos disponibles, desea ver los de la siguiente semana? 1-Si 2-No");
 	}
 	public static void getError2() {
-		System.out.println(error2);
+		System.out.println("Datos no confirmados.");
 	}
 	public static void getError3() {
-		System.out.println(error3);	
+		System.out.println("El numero de turno ingresado no existe.");	
 	}
 	public static void getError4() {
-		System.out.println(error4);	
+		System.out.println("No hay turnos disponibles.");	
 	}
 	public static void getError5() {
-		System.out.println(error5);	
+		System.out.println("El medico seleccionado no existe.");	
 	}
 	public static void getError6() {
-		System.out.println(error6);	
+		System.out.println("No hay medicos disponibles.");	
 	}
 	public static void getError7() {
-		System.out.println(error7);	
+		System.out.println("La opcion ingresada no es valida.");	
 	}
 	public static void getError8(int i) {
-		System.out.println(error8 + i + " de 3 intentos");
+		System.out.println("Usuario y contraseña invalidos. " + i + " de 3 intentos");
 	}
 	
 	
