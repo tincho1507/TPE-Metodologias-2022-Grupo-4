64b9a503826dab3e6423e282af83e9a2d040866f
Juan Francisco Herrero <82331755+juanfraherrero@users.noreply.github.com>
2022-07-04 15:36:30 -0300

    Modificado el Error.java
    ahora en vez de tener un método por cada error, tenemos un método que se le pasa que tipo de error es por parámetro.

--- Sprint 2/Codigo Fuente/src/Errores/Error.java
+++ Sprint 2/Codigo Fuente/src/Errores/Error.java
@@ -2,33 +2,43 @@ package Errores;
 
 public class Error {
 	
-	public static void getError0() {
-		System.out.println("El medico no trabaja en la obra social tuya.");
-	}
+	public static void getError(int error) {
+		switch (error) {
+			case 0 : 
+				System.out.println("El medico no trabaja en la obra social tuya.");
+				break;
+			case 1 : 
+				System.out.println("No hay turnos disponibles, desea ver los de la siguiente semana? 1-Si 2-No");
+				break;
+			case 2 : 
+				System.out.println("Datos no confirmados.");
+				break;
+			case 3 : 
+				System.out.println("El numero de turno ingresado no existe.");
+				break;
+			case 4 : 
+				System.out.println("No hay turnos disponibles.");
+				break;
+			case 5 : 
+				System.out.println("El medico seleccionado no existe.");
+				break;
+			case 6 : 
+				System.out.println("No hay medicos disponibles.");
+				break;
+			case 7 :
+				System.out.println("La opcion ingresada no es valida.");
+				break;				
+		}
 	
-	public static void getError1() {
-		System.out.println("No hay turnos disponibles, desea ver los de la siguiente semana? 1-Si 2-No");
+	
+	
 	}
-	public static void getError2() {
-		System.out.println("Datos no confirmados.");
-	}
-	public static void getError3() {
-		System.out.println("El numero de turno ingresado no existe.");	
-	}
-	public static void getError4() {
-		System.out.println("No hay turnos disponibles.");	
-	}
-	public static void getError5() {
-		System.out.println("El medico seleccionado no existe.");	
-	}
-	public static void getError6() {
-		System.out.println("No hay medicos disponibles.");	
-	}
-	public static void getError7() {
-		System.out.println("La opcion ingresada no es valida.");	
-	}
-	public static void getError8(int i) {
-		System.out.println("Usuario y contraseña invalidos. " + i + " de 3 intentos");
+	public static void getError(int error, int i) {
+		switch (error) {
+			case 8 :
+				System.out.println("Usuario y contraseña invalidos. " + i + " de 3 intentos");
+				break;
+		}
 	}
 	
 	
